32bc9457acca4824383d94bc1cae1936f0279e9e
Alexander Morton <alexander.david.morton.cern.ch>
2016-10-03 11:24:22 +0200

    Snapshot update.

--- Validation/SiPixelPhase1RecHitsV/interface/SiPixelPhase1RecHitsV.h
+++ Validation/SiPixelPhase1RecHitsV/interface/SiPixelPhase1RecHitsV.h
@@ -10,6 +10,7 @@
 // Additional Authors: Alexander Morton - modifying code for validation use
 
 #include "Validation/SiPixelPhase1CommonV/interface/SiPixelPhase1BaseV.h"
+
 #include "DataFormats/TrackerRecHit2D/interface/SiPixelRecHitCollection.h"
 #include "SimTracker/TrackerHitAssociation/interface/TrackerHitAssociator.h"
 
